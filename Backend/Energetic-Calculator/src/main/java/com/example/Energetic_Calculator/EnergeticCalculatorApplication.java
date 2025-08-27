34176aa5a16046374e5181909a15dc984b07fc3a
Sara Castañeda <saracastanedaecheverri@gmail.com>
2025-08-27 15:16:50 -0500

    First commit

---
+++ Backend/Energetic-Calculator/src/main/java/com/example/Energetic_Calculator/EnergeticCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Energetic_Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergeticCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergeticCalculatorApplication.class, args);
+	}
+
+}
---
+++ Backend/Energetic-Calculator/src/test/java/com/example/Energetic_Calculator/EnergeticCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Energetic_Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergeticCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
